709e53bb370febc477f2226fc92eabaa3d9dd8bf
Piranha <franck.chevallier@juloa.com>
2015-12-08 12:13:04 +0100

    starting project

---
+++ models/user.js
@@ -0,0 +1,24 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var userSchema = mongoose.Schema({
+    username: String
+
+});
+
+// on every save, add the date
+userSchema.pre('save', function(next) {
+    // get the current date
+    var currentDate = new Date();
+
+    // change the updated_at field to current date
+    this.updated_at = currentDate;
+
+    // if created_at doesn't exist, add to that field
+    if (!this.created_at)
+        this.created_at = currentDate;
+
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
---
+++ server.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 2096;
+var app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://10.104.14.112/bharlemshake');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log("Server connectd to mongodb");
+});
+
+var User = require('./models/user');
+
+var router = express.Router();
+
+router.route("/users")
+
+    .post(function (req, res) {
+        var user = new User();
+        console.log(req.body);
+        user.username = req.body.username;
+        user.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({message: 'User created !'});
+        });
+    })
+    .get(function (req, res) {
+       User.find(function (err, bears) {
+            if (err)
+                res.send(err);
+
+            res.json(bears);
+        });
+    });
+
+// Start server
+
+app.get('/', function (req, res) {
+    res.send("Salut");
+});
+
+app.use("/api", router);
+
+app.listen(port);
+
